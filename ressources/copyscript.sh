ce6e8d3ae89f28394adbf988f88500d07cce5c67
Lenard Frommelt <lenny09918050@googlemail.com>
2021-06-14 10:25:38 +0200

    initial commit

---
+++ ressources/copyscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+##
+cp sources/ressources/.gitignore output/.gitignore
+cp sources/ressources/LICENSE output/LICENSE
+cp sources/ressources/UNLICENSE output/UNLICENSE
+cp sources/ressources/package.json output/package.json
+cp sources/ressources/README.md output/README.md
+
+echo 0
---
+++ source/index/index.coffee
@@ -0,0 +1,3 @@
+Object.defineProperty(exports, "__esModule", { value: true })
+
+exports.helloWorld = -> console.log("Hello World!")
